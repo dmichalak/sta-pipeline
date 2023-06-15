678ac59d2a8dfcdf4cad844eb0f19fd5dee4e305
dmichalak <dennisjmichalak@gmail.com>
2023-06-15 15:30:25 -0400

    add more scripts

---
+++ relion/relion_tomo_import_tomograms.sh
@@ -0,0 +1,1 @@
+relion_tomo_import_tomograms --i tomograms_descr.star --o tomogram_set.star --hand 1 --angpix 1.083 --voltage 300 --Cs 2.7 --Q0 0.07 --fd 2.4 --nc newst.com --tc tilt.com --ali wws1_ts002_ali.mrc
