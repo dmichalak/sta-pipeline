73c19ae7e301c33d68ff9f48664a130ae47d5931
dmichalak <dennisjmichalak@gmail.com>
2023-07-21 13:00:55 -0400

    make cleaning particles clearer

--- src/sta_pipeline/misc/model2star.py
+++ src/sta_pipeline/misc/model2star.py
@@ -10,7 +10,7 @@ import starfile
 
 
 # Set the following variables
-path_to_model_directory = "./data/models" # Path to the directory containing the models (in .mod format)
+path_to_model_directory = "/data/singal/cryo-ET-recon/models" # Path to the directory containing the models (in .mod format)
 randomize_particle_angles = True # If True, randomize the angles of the particles. If False, set all angles to 0.0
 
 
@@ -24,33 +24,39 @@ randomize_particle_angles = True # If True, randomize the angles of the particle
 #######################
 
 # Read all model files in the directory and store them in a dictionary
-path_to_model_directory = Path(path_to_model_directory).resolve() 
+path_to_model_directory = Path(path_to_model_directory).absolute() 
 model_dict = {}
 for model in path_to_model_directory.glob("*.mod"):
     model_dict[model.stem] = imodmodel.read(model)
     model_dict[model.stem]["name"] = model.stem
 
-# Create a star file for each model file
-star_dict = {}
-for key in list(model_dict.keys()):
-    star_dict[key] = pd.DataFrame()
-    star_dict[key]["rlnTomoName"] = model_dict[key]["name"]
-    star_dict[key]["rlnCoordinateX"] = model_dict[key]["x"]
-    star_dict[key]["rlnCoordinateY"] = model_dict[key]["y"]
-    star_dict[key]["rlnCoordinateZ"] = model_dict[key]["z"]
+# Check if any models were found at path_to_model_directory
+if len(model_dict) == 0:
+    raise ValueError("No model files found in the directory: ", path_to_model_directory)
+else:
+    # Create a star file for each model file
+    star_dict = {}
 
-    # Set the angles of the particles
-    if randomize_particle_angles == True:
-        star_dict[key]["rlnAngleRot"] = np.random.uniform(0, 180, len(model_dict[key]))
-        star_dict[key]["rlnAngleTilt"] = np.random.uniform(0,180, len(model_dict[key]))
-        star_dict[key]["rlnAnglePsi"] = np.random.uniform(0, 180, len(model_dict[key]))
-    else:
-        star_dict[key]["rlnAngleRot"] = 0.0
-        star_dict[key]["rlnAngleTilt"] = 0.0
-        star_dict[key]["rlnAnglePsi"] = 0.0 
+    for key in list(model_dict.keys()):
+        star_dict[key] = pd.DataFrame()
+        star_dict[key]["rlnTomoName"] = model_dict[key]["name"]
+        star_dict[key]["rlnCoordinateX"] = model_dict[key]["x"]
+        star_dict[key]["rlnCoordinateY"] = model_dict[key]["y"]
+        star_dict[key]["rlnCoordinateZ"] = model_dict[key]["z"]
 
-# Write the star files for each model found in the directory
-for key in star_dict.keys():
-    starfile.write(star_dict[key], path_to_model_directory / f"{star_dict[key]['rlnTomoName'][0]}.star", overwrite=True)
+        # Set the angles of the particles
+        if randomize_particle_angles == True:
+            star_dict[key]["rlnAngleRot"] = np.random.uniform(0, 180, len(model_dict[key]))
+            star_dict[key]["rlnAngleTilt"] = np.random.uniform(0,180, len(model_dict[key]))
+            star_dict[key]["rlnAnglePsi"] = np.random.uniform(0, 180, len(model_dict[key]))
+        else:
+            star_dict[key]["rlnAngleRot"] = 0.0
+            star_dict[key]["rlnAngleTilt"] = 0.0
+            star_dict[key]["rlnAnglePsi"] = 0.0 
 
-print("Done!")
+    # Write the star files for each model found in the directory
+    for key in star_dict.keys():
+        starfile.write(star_dict[key], path_to_model_directory / f"{star_dict[key]['rlnTomoName'][0]}.star", overwrite=True)
+        print("Star file written at: ", path_to_model_directory / f"{star_dict[key]['rlnTomoName'][0]}.star")
+    
+    print("Done!")
